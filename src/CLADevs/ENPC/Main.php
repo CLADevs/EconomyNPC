af2851bac01af885c0db046869271c3006d2be30
iiFlamiinBlaze <iiflamiinblaze123@gmail.com>
2019-02-01 14:18:03 -0500

    Little fix up

--- src/CLADevs/ENPC/Main.php
+++ src/CLADevs/ENPC/Main.php
@@ -4,50 +4,51 @@ declare(strict_types=1);
 
 namespace CLADevs\ENPC;
 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\entity\Entity;
-use pocketmine\nbt\tag\{
-	IntTag, StringTag
-};
-use pocketmine\utils\TextFormat as C;
-use pocketmine\command\{
-	Command, CommandSender
-};
+use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase{
 
-	public function onEnable(): void{
+	public function onEnable() : void{
 		Entity::registerEntity(NPC::class, true);
-		$this->getLogger()->info("Everything is ready!");
+		$this->getLogger()->info("EconomyNPC has been enabled!");
 	}
 
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
-		if($sender->isOp() == false){
-			$sender->sendMessage(C::RED . "You do not have pemission to use this command");
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		if(!$sender->isOp()){
+			$sender->sendMessage(TextFormat::RED . "You do not have permission to use this command");
+			return false;
+		}
+		if(!$sender instanceof Player){
+			$sender->sendMessage(TextFormat::RED . "Use this command in-game");
 			return false;
 		}
 		if(count($args) < 1){
-			$sender->sendMessage("Usage: /e <1:2:3>");
+			$sender->sendMessage(TextFormat::GRAY . "Usage: /enpc <1:2:3>");
 			return false;
 		}
-
 		switch($args[0]){
 			case "1":
 			case "2":
 			case "3":
-			$nbt = Entity::createBaseNBT($sender);
-			$nbt->setTag($sender->namedtag->getTag("Skin"));
-			$nbt->setTag(new IntTag("position", (int)$args[0]));
-			$nbt->setTag(new StringTag("player", "0"));
-
-			$npc = new NPC($sender->getLevel(), $nbt);
-			$npc->setNameTag(C::RED . "Unknown");
-			$npc->setNameTagAlwaysVisible(true);
-			$npc->spawnToAll();
-			break;
+				$nbt = Entity::createBaseNBT($sender);
+				$nbt->setTag($sender->namedtag->getTag("Skin"));
+				$nbt->setTag(new IntTag("position", (int)$args[0]));
+				$nbt->setTag(new StringTag("player", "0"));
+				$npc = new NPC($sender->getLevel(), $nbt);
+				$npc->setNameTag(TextFormat::RED . "Unknown");
+				$npc->setNameTagAlwaysVisible(true);
+				$npc->spawnToAll();
+				break;
 			default:
-			$sender->sendMessage("Usage: /e <1:2:3>");
-			break;
+				$sender->sendMessage(TextFormat::GRAY . "Usage: /enpc <1:2:3>");
+				break;
 		}
 		return true;
 	}
--- src/CLADevs/ENPC/NPC.php
+++ src/CLADevs/ENPC/NPC.php
@@ -4,17 +4,16 @@ declare(strict_types=1);
 
 namespace CLADevs\ENPC;
 
+use pocketmine\entity\Human;
 use pocketmine\level\Level;
-use pocketmine\utils\TextFormat as C;
-use pocketmine\nbt\tag\{
-	CompoundTag, StringTag
-};
-use pocketmine\entity\{
-	Human, Skin
-};
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat;
 
 class NPC extends Human{
 
+	/** @var null|Plugin $eco */
 	protected $eco;
 
 	public function __construct(Level $lvl, CompoundTag $nbt){
@@ -22,44 +21,36 @@ class NPC extends Human{
 		parent::__construct($lvl, $nbt);
 	}
 
-	public function getPos(): int{
+	public function getPos() : int{
 		return $this->namedtag->getInt("position");
 	}
 
-	public function getPlayer(): string{
+	public function getPlayer() : string{
 		return $this->namedtag->getString("player");
 	}
 
-	public function Data(): void{
-		$moneys = $this->eco->getAllMoney();
-		$i = 1;
-		arsort($moneys);
-
-		foreach($moneys as $name => $money){
-			if($i < 4){
-				if($i == $this->getPos()) $this->namedtag->setTag(new StringTag("player", $name));
-				$i++;
-			}
-		}
-	}
-
-	public function entityBaseTick(int $tick = 1): bool{
-		$base = parent::entityBaseTick($tick);
-
+	public function entityBaseTick(int $tick = 1) : bool{
 		if(count($this->eco->getAllMoney()) > 0){
-			$this->Data();
-			$this->setNameTag(C::GRAY . $this->getPos() . ". " . C::WHITE . $this->getPlayer() . ": " . C::YELLOW . $this->eco->myMoney($this->getPlayer()));
-
-			#i fudged up skin xd help me by pull request
-			// $nbt = $this->getLevel()->getServer()->getOfflinePlayerData($this->getPlayer());
-			// $s = $nbt->getTag("Skin");
-			// $skin = new Skin($s->getString("Name"), $s->getByteArray("Data"), $s->getByteArray("CapeData"), $s->getString("GeometryName"), $s->getByteArray("GeometryData"));
-			// $s = $nbt->getTag("Skin");
-			// $this->namedtag->setTag($s);
-			// $this->spawnToAll();
+			$moneys = $this->eco->getAllMoney();
+			$i = 1;
+			arsort($moneys);
+			foreach($moneys as $name => $money){
+				if($i < 4){
+					if($i == $this->getPos()) $this->namedtag->setTag(new StringTag("player", $name));
+					$i++;
+				}
+			}
+			$this->setNameTag(TextFormat::GRAY . $this->getPos() . ". " . TextFormat::WHITE . $this->getPlayer() . ": " . TextFormat::YELLOW . $this->eco->myMoney($this->getPlayer()));
+			#I fudged up skin xd help me by pull request
+			/*$nbt = $this->getLevel()->getServer()->getOfflinePlayerData($this->getPlayer());
+			$s = $nbt->getTag("Skin");
+			$skin = new Skin($s->getString("Name"), $s->getByteArray("Data"), $s->getByteArray("CapeData"), $s->getString("GeometryName"), $s->getByteArray("GeometryData"));
+			$s = $nbt->getTag("Skin");
+			$this->namedtag->setTag($s);
+			$this->spawnToAll();*/
 		}else{
-			$this->setNameTag(C::RED . "Unknown");
+			$this->setNameTag(TextFormat::RED . "Unknown");
 		}
-		return $base;
+		return parent::entityBaseTick($tick);
 	}
 }
